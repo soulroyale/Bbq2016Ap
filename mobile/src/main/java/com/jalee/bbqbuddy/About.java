a44a18e1c1cb79e2641c66f027b3fd83412d3006
Aaron Walker <Aaron.Walker@live.com.au>
2016-01-25 18:47:57 +1000

    Initial Commit

---
+++ mobile/src/main/java/com/jalee/bbqbuddy/About.java
@@ -0,0 +1,20 @@
+package com.jalee.bbqbuddy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class About extends AppCompatActivity {
+
+
+    public void closeMe (View view) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
---
+++ mobile/src/paid/java/com/jalee/bbqbuddy/Constants.java
@@ -0,0 +1,13 @@
+package com.jalee.bbqbuddy;
+
+/**
+ * Created by Aaron on 23/01/2016.
+ */
+public class Constants {
+
+    public enum Type {
+        FREE, PAID;
+    }
+
+    public static final Type type = Type.PAID;
+}
