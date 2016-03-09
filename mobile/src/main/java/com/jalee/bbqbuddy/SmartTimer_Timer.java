aeb323cc17fa4698ce74b684c0174f3e1b4c2e4e
Aaron Walker - Work PC <aaron.walker@live.com.au>
2016-03-09 11:52:02 +1000

    - add Landscape Layout for Timer

--- mobile/src/main/java/com/jalee/bbqbuddy/SmartTimer_Timer.java
+++ mobile/src/main/java/com/jalee/bbqbuddy/SmartTimer_Timer.java
@@ -1,24 +1,14 @@
 package com.jalee.bbqbuddy;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
 
 import java.io.Serializable;
 import java.util.List;
@@ -44,8 +34,8 @@ public class SmartTimer_Timer extends Fragment {
             bannerAdView.loadAd(bannerAdRequest);
             return v;
         } else {
-            View v = inflater.inflate(R.layout.fragment_smart_timer_timer_landscape, container, false);
-            View rootView = inflater.inflate(R.layout.fragment_smart_timer_timer_landscape, container, false);
+            View v = inflater.inflate(R.layout.fragment_smart_timer_timer_LS, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_smart_timer_timer_LS, container, false);
             AdView bannerAdView = (AdView) rootView.findViewById(R.id.adViewTimer);
             AdRequest bannerAdRequest = new AdRequest.Builder()
                     .addTestDevice("63477755EE05E10016CC8C5A71F18B64")
